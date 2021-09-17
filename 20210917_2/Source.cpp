b5514a6da731bf3c05c0ba1e401147e8feed35d6
최필립 <vlfflq48@naver.com>
2021-09-17 15:32:46 +0900

    First Change

---
+++ 20210917_2/Source.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//배열
+	int a[100];
+	int i = 0;
+	int Sum = 0;
+	//a 0 1 2 3 4 5 6 7 8 9 
+	// [2^32][][][][][][][][][]
+	//for (i = 0; i <= 100; i++)
+	//{
+	//	Sum += i;
+	//}
+
+	//cout << Sum;
+
+	for (i = 0; i <= 100; i + 3)
+	{
+		Sum = Sum + i;
+	}
+
+	cout << Sum;
+
+
+	//type
+	//int -> float
+	//float -> int
+	//float <- double 
+	//casting(형변환)
+	//(형)원래형태
+	//static_cast<새로운형>(원래형태)
+	//int -> char
+	//char -> int
+	
+
+	//char C = static_cast<char>(512);
+	//cout << static_cast<int>(C) << endl;
+
+	//cin >> Korea;
+	//cin >> England;
+	//cin >> Brazil;
+	//int Sum = Korea + England + Brazil;
+	//float Average = static_cast<float>((Korea + England + Brazil)) / 3.0f;
+	//cout << Sum << endl;
+	//cout << Average << endl;
+
+	return 0;
+}
